72bb74d8357da0e3dbbc2ed92b0688ffb11428b0
sixit <249517548@qq.com>
2021-08-18 23:01:04 +0800

    我的第一个java代码

---
+++ java-info/src/main/java/com/sixit/java/project/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.sixit.java.project;
+
+/**
+ * 我的第一个java程序
+ *
+ * @author sixit 249517548@qq.com
+ * @version 2021-08-18 22:57
+ * @since JDK11
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("HelloWorld for WIndows10 git Maven IDEA");
+    }
+}
